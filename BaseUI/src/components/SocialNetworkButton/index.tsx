34d4006f3fc1805140f220675a10b396cb747431
Gergely Laszlo Csepanyi <gergely-laszlo.csepanyi@nixs.com>
2023-04-20 16:36:57 +0200

    Finished debugging after review.

--- BaseUI/src/components/SocialNetworkButton/index.tsx
+++ BaseUI/src/components/SocialNetworkButton/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {TouchableOpacity, Image} from 'react-native';
+import {TouchableOpacity, Image, ImageSourcePropType} from 'react-native';
 import SocialNetworkButtonStyles from './styles';
 
 interface SocialNetworkButtonProps {
-  image: any;
+  image: ImageSourcePropType;
 }
 
 class SocialNetworkButton extends React.Component<SocialNetworkButtonProps> {
